553b26296e262903e6d6fc5ec14b9e3783ce2808
jsw <develjsw@gmail.com>
2024-02-29 23:59:09 +0900

    config/typeorm module 적용, 환경별 config 값 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configDevelopment from './config/config.development';
+import configProduction from './config/config.production';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+let config;
+if (process.env.NODE_ENV === 'production') {
+    config = configProduction;
+} else {
+    config = configDevelopment;
+}
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+            load: [config]
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) =>
+                // TODO : config/config.development.ts 파일에 type 생성 후, object 타입 변경 예정
+                configService.get<object>('config-info.database.mysql'),
+            inject: [ConfigService]
+        })
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
---
+++ src/config/config.development.ts
@@ -0,0 +1,26 @@
+import { registerAs } from '@nestjs/config';
+import * as fs from 'fs';
+import * as path from 'path';
+
+// TODO : secret 위치 변경 필요
+const MYSQL_HOST = path.resolve('./secret/development/mysql-host');
+const MYSQL_USER_NAME = path.resolve('./secret/development/mysql-username');
+const MYSQL_PASSWORD = path.resolve('./secret/development/mysql-password');
+
+export default registerAs('config-info', () => ({
+    port: parseInt(process.env.PORT, 10) || 8003,
+
+    database: {
+        mysql: {
+            type: 'mysql',
+            host: fs.readFileSync(MYSQL_HOST).toString(),
+            port: 3306,
+            username: fs.readFileSync(MYSQL_USER_NAME).toString(),
+            password: fs.readFileSync(MYSQL_PASSWORD).toString(),
+            database: 'test',
+            entities: ['dist/!**!/entities/!*.entity{.ts,.js}'],
+            synchronize: false,
+            logging: 'all'
+        }
+    }
+}));
